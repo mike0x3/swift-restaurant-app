acf279d89a13d332f06f5ed3d22714a9659e7e41
Mykaylo Chornenkiy <tropea2004@gmail.com>
2021-08-26 17:04:41 +0200

    Initial Commit

---
+++ TabNavigationTest/TabNavigationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabNavigationTestApp.swift
+//  TabNavigationTest
+//
+//  Created by Uno qualunque on 26/08/21.
+//
+
+import SwiftUI
+
+@main
+struct TabNavigationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
